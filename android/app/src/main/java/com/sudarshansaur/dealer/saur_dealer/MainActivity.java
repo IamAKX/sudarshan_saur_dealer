c27df10df67e1837a826e2e30b6df0f4c8f85273
Akash Giri <akx.sonu@gmail.com>
2023-06-28 15:55:09 +0530

    Android fix

--- android/app/src/main/java/com/sudarshansaur/dealer/saur_dealer/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.sudarshansaur.dealer.saur_dealer;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/sudarshansaur/dealer/saur_dealer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sudarshansaur.dealer.saur_dealer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
